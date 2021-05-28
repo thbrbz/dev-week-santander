d75d88f69cab0cdfc54cfa95e48bff2bfcfdbeec
Thiago Barboza <thbrbz@gmail.com>
2021-05-28 20:59:29 -0300

    Arrumando bug na classe service.

--- src/main/java/com/example/bootcamp/service/StockService.java
+++ src/main/java/com/example/bootcamp/service/StockService.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.bootcamp.exception.BusinessException;
@@ -17,6 +18,7 @@ import com.example.bootcamp.model.dto.StockDTO;
 import com.example.bootcamp.repository.StockRepository;
 import com.example.bootcamp.util.MessageUtils;
 
+@Service
 public class StockService {
 
 	@Autowired
